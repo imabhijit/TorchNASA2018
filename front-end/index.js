d7db165b2397816ab98f570a3cc072a352209141
Abhijit Gupta <abhijit.gupta1122@gmail.com>
2018-10-20 17:02:15 -0400

    -Making a JSOn file and reading from it

--- front-end/index.js
+++ front-end/index.js
@@ -1,12 +1,16 @@
 //Import twit module
 var Twit = require('twit');
 
+var fs = require('fs');
+
 var keys = require('./keysConfig');
 
 var twitter = new Twit(keys);
 
+var obj = {table: []};
+
 function getUpdates() {
-    twitter.get('search/tweets', {q: '#nasatorch', count: 2}, function (err, data, response) {
+    twitter.get('search/tweets', {q: '#nasatorch', count: 4}, function (err, data, response) {
         var torchArr = [];
         data.statuses.forEach(tweet => {
             let longitude = null;
@@ -27,19 +31,47 @@ function getUpdates() {
             }
 
             let t1 = {msg: tweet.text, lng: longitude, lat: lattitude, img: image, time: tweet.created_at};
-            // console.log("torchArr Before------------------------------" + JSON.stringify(torchArr));
-            torchArr.push(t1);
-            // console.log("torchArr After------------------------------" + JSON.stringify(torchArr));
 
+            torchArr.push(t1);
+
+            // Clean table
+            // json = JSON.stringify(obj); //convert it back to json
+            // fs.writeFile('test.json', json, 'utf8', function writeCallback(err){
+            //     if(err){
+            //         console.log(err);
+            //     }
+            // });
+
+            // fs.readFile('test.json', 'utf8', function readFileCallback(err, data){
+            //     if (err){
+            //         console.log(err);
+            //     } else {
+            //         obj = JSON.parse(data); //now it an object
+            //         obj.table.push(t1); //add some data
+            //         json = JSON.stringify(obj); //convert it back to json
+            //         fs.writeFile('test.json', json, 'utf8', function writeCallback(err){
+            //             if(err){
+            //                 console.log(err);
+            //             }
+            //         }); // write it back
+            //     }
+            // });
+        });
+        json = JSON.stringify(torchArr); //convert it back to json
+        fs.writeFile('test.txt', json, 'utf8', function writeCallback(err){
+            if(err){
+                console.log(err);
+            }
         });
         // return torchArr;
-        return [
-            {msg: 'hello mk1', lng: -73.7124, lat: 45.6066, img: null, time: "101"},
-            {msg: 'hello mk2', lng: -74.59124, lat: 46.11066, img: null, time: "101"},
-            {msg: 'hello mk3', lng: -79.7124, lat: 43.6066, img: null, time: "101"}
-        ];
+        // return [
+        //     {msg: 'hello mk1', lng: -73.7124, lat: 45.6066, img: null, time: "101"},
+        //     {msg: 'hello mk2', lng: -74.59124, lat: 46.11066, img: null, time: "101"},
+        //     {msg: 'hello mk3', lng: -79.7124, lat: 43.6066, img: null, time: "101"}
+        // ];
     });
 }
 
 //For Dev (Actual delay = 1000*60*5)
 // setInterval(test, 5000);
+getUpdates();
