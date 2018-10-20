ba11115be7947a64c70a38e4ef564a83404ccbf7
Abhijit Gupta <abhijit.gupta1122@gmail.com>
2018-10-20 07:55:51 -0400

    -Get Location, tweet message and Image

--- front-end/twitTest1.js
+++ front-end/twitTest1.js
@@ -5,14 +5,27 @@ var keys = require('./keysConfig');
 
 var twitter = new Twit(keys);
 
-twitter.get('search/tweets', { q: '#nasatorch', count: 25 }, function(err, data, response) {
-    data.statuses.forEach(tweet => {
-        if (tweet.coordinates != null){
-        longitude = tweet.coordinates.coordinates[0];
-        lattitude = tweet.coordinates.coordinates[1];
+function getUpdates() {
+    twitter.get('search/tweets', {q: '#nasatorch', count: 1}, function (err, data, response) {
+        data.statuses.forEach(tweet => {
+            if (tweet.coordinates != null) {
+                longitude = tweet.coordinates.coordinates[0];
+                lattitude = tweet.coordinates.coordinates[1];
 
-        console.log("Message: \'" + tweet.text + "\'");
-        console.log(" The Coordinates are Lattitude: " + lattitude + ", Longitude: " + longitude);
-        }
+                console.log("Message: \'" + tweet.text + "\'");
+                console.log("The Coordinates are Lattitude: " + lattitude + ", Longitude: " + longitude);
+                if (tweet.entities.media != undefined) {
+                    let image = tweet.entities.media[0];
+                    console.log("Image: " + image.media_url);
+                }
+                console.log("Time: " + tweet.created_at);
+                console.log("-----------------------------------------")
+            }
+        });
     });
-});
+}
+//Actual
+// setInterval(getUpdates, 1000*60*5);
+
+//For Dev
+setInterval(getUpdates, 1000);
