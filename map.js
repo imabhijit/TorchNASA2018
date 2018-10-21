736b09e85e2261c369f82f54c9243e5441d5a7c4
Lara Tran <larat248@gmail.com>
2018-10-21 11:57:14 -0400

    -resize images popup

--- map.js
+++ map.js
@@ -46,10 +46,10 @@ $.getJSON("./test.txt", function(json) {
       }
       markers[i] = L.marker( [ torchArr[i].lat, torchArr[i].lng],{icon:myIcon}).addTo(map);
       if(torchArr[i].img){
-        markers[i].bindPopup("<p>" + torchArr[i].msg+"</p> <p>" + torchArr[i].time+ "</p><img id =  &quotimages&quot src= "+ torchArr[i].img +">",{maxWidth: "auto"
+        markers[i].bindPopup("<p>" + torchArr[i].msg+"</p> <p>" + torchArr[i].time+ "</p><img src= "+ torchArr[i].img +">",{maxWidth: "60"
 }).openPopup();
       }else {
-        markers[i].bindPopup("<p>" + torchArr[i].msg+"</p> <p>" + torchArr[i].time+ "</p>",{maxWidth: "auto"}).openPopup();
+        markers[i].bindPopup("<p>" + torchArr[i].msg+"</p> <p>" + torchArr[i].time+ "</p>",{maxWidth: "300"}).openPopup();
       }
     }
   }
